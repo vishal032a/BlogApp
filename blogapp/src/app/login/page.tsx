299930639e6b0ab399b51fe89c371393d19abfbb
vishal <vk7821188@gmail.com>
2024-08-14 12:27:13 +0530

    finished with the login ui

--- blogapp/src/app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import Login from "@/components/Login";
-const page = () => {
-  return (
-  <Login/>
-  );
-};
-
-export default page;
--- blogapp/src/app/page.tsx
+++ blogapp/src/app/page.tsx
@@ -1,5 +1,12 @@
+import Login from "@/components/Login";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="">hello</main>;
+  return (
+    <main className="">
+      <div style={{ marginTop: 64 }}>
+        <Login />
+      </div>
+    </main>
+  );
 }
